2d84c25d8fea6732f696b064ccf81f41dd28a938
Nikolay Popov <ponikan@gmail.com>
2024-07-21 01:03:07 +0300

    commit works program of getting power values

--- src/main/java/Constant.java
+++ src/main/java/Constant.java
@@ -1,9 +1,5 @@
 public class Constant {
-    //public static final String FILENAME_IN = "C:\\Users\\user\\IdeaProjects\\Power\\texts\\MML.txt";
-//    public static final String FILENAME_IN = "C:\\Users\\user\\Downloads";
-//    public static final String FILENAME_OUT = "C:\\Users\\user\\IdeaProjects\\Power\\texts\\MML_PowerOut.txt";
-//    public static final String FILENAME_POWER_OF = "C:\\Users\\user\\IdeaProjects\\Power\\texts\\MML_PowerOff.txt";
-//    public static final String FILENAME_POWER_MISTAKES = "C:\\Users\\user\\IdeaProjects\\Power\\texts\\MML_Mistakes.txt";
+
         public static final Double TRESHOLD_POWER_VOLTAGE = 49.7;
         public static final String FILENAME_IN = "C:\\Users\\user\\Downloads";
         public static final String FILENAME_OUT = "C:\\Users\\user\\Desktop\\MML_OUTS\\PowerAll.txt";
